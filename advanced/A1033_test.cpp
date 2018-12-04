7e72d8b8c10aa5efea901de820d2ddceab00c1c9
balance <32612349+justforbalance@users.noreply.github.com>
2018-12-04 21:56:06 +0800

    create some files

---
+++ advanced/A1033_test.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<math.h>
+
+using namespace std;
+
+struct Node
+{
+	double price;
+	double distance;
+}stations[510];//表示油价以及距离杭州的距离
+
+bool cmp(Node a, Node b)
+{
+	return a.distance<b.distance;
+};
+
+int main(void)
+{
+	double c, Dmax, Dave;
+	int n;//c表示油箱总容量，Dmax代表总距离，Dave代表每单位油支持的距离，n代表加油站数量
+
+		  //scanf("%d%d%d%d", &c, &Dmax, &Dave, &n);
+	cin >> c >> Dmax >> Dave >> n;
+	for (int i = 0; i<n; ++i)
+	{
+		//scanf("%lf%d", &stations[i].price, &stations[i].distance);
+		cin >> stations[i].price >> stations[i].distance;
+	}
+	sort(stations, stations + n, cmp);
+	stations[n].price = 0;
+	stations[n].distance = Dmax;
+
+	double total_price = 0.00;
+	double adding[510] = { 0 };//表示每一个加油站的加油数量；
+	double left_gas = 0;//表示当前剩余油的数量
+	double passed_dis = 0;//表示已经跑的距离
+	double next_dist = 0;//表示距离下一站的距离
+	double max_gas_dis = c*Dave;
+
+	int next_st = 0;
+	int pass_node = 0;
+	//整体策略为：首先保证邮箱中的油可以跑到下一个站，如果加满也不能到达下一站，那么结束，输出跑的距离
+	//          其次，如果油价比下下一站的贵，那么不加
+	//                 如果比下一站的便宜，那就加油，要么加到可以到达更便宜的站点，要么加满。
+	//注意passed_dis的更新；
+	if (stations[0].distance != 0)
+	{
+		printf("The maximum travel distance = %.2f", passed_dis);
+		cin >> n;
+		return 0;
+	}
+	for (int i = 0; i<n; i = next_st)
+	{
+		pass_node = 0;
+		passed_dis = stations[i].distance;
+
+		for (int j = i; j<n + 1 && (stations[j].distance - stations[i].distance) <= max_gas_dis; ++j)
+		{
+			pass_node = j - i;
+		}
+
+		if (pass_node == 0)
+		{
+			printf("The maximum travel distance = %.2f", passed_dis + max_gas_dis);
+			cin >> n;
+			return 0;
+		}
+
+		next_st = i + 1;
+		adding[i] = c - left_gas;
+		for (int k = 1; k <= pass_node; ++k)
+		{
+			next_dist = stations[i + k].distance - stations[i].distance;
+			if (stations[i + k].price<stations[i].price)
+			{
+				next_st = i + k;
+				adding[i] = next_dist / Dave - left_gas;
+				break;
+			}
+
+			if (stations[i + k].price<stations[next_st].price)
+			{
+				next_st = i + k;
+				adding[i] = c - left_gas;
+			}
+		}
+		left_gas += adding[i];
+		left_gas -= (stations[next_st].distance - stations[i].distance) / Dave;
+
+	}
+
+	for (int i = 0; i < n + 1; ++i)
+	{
+		cout << "station " << i << " " << stations[i].distance << endl;
+	}
+
+	for (int i = 0; i<n; ++i)
+	{
+		cout << "station" << i << "'s adding is" << adding[i] << endl;
+		total_price = total_price + adding[i] * stations[i].price;
+	}
+
+	//printf("%.2f\n", total_price);
+	cout << total_price;
+	cin >> n;
+	return 0;
+}
