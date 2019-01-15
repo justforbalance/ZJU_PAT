edddfac1adfb04d8b2cb356f4c8b6c6c1a43de2c
balance <32612349+justforbalance@users.noreply.github.com>
2019-01-15 16:47:52 +0800

    Update A1103.cpp

--- advanced/A1103.cpp
+++ advanced/A1103.cpp
@@ -1,9 +1,15 @@
 #include<cstdio>
 #include<iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+//169 5 2
+vector<int> mi;
 
 int main(void)
 {
+    int n, p, k;
+    scanf("%d%d%d", &n, &k, &p);
     
 }
