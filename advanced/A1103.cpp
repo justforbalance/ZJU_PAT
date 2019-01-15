f6bd69035ffd092a04982745b029ce6be82e004f
balance <32612349+justforbalance@users.noreply.github.com>
2019-01-15 17:14:29 +0800

    Update A1103.cpp

--- advanced/A1103.cpp
+++ advanced/A1103.cpp
@@ -7,9 +7,34 @@ using namespace std;
 //169 5 2
 vector<int> mi;
 
+int n, p, k;
+int maxdisum = -1;
+vector<int> di_mi, ans, temp;
+
+int power_p(int x){
+    int res=1;
+    for (int i = 1; i <= p;++i)
+    {
+        res = res * x;
+    }
+    return res;
+};
+
+void init(){
+    //int tmp;
+    for (int i = 0; power_p(i) < n;++i)
+    {
+        di_mi.push_back(power_p(i));
+    }
+};
+
+void DFS(int index, int nowk, int now_sum, int nowdisum){
+
+};
+
 int main(void)
 {
-    int n, p, k;
     scanf("%d%d%d", &n, &k, &p);
+
     
 }
