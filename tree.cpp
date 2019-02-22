6168b5fa937f7bbc616e02a94f79a5b890a12d35
balance <32612349+justforbalance@users.noreply.github.com>
2019-02-22 15:34:28 +0800

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -317,10 +317,111 @@
     二叉查找树的中序遍历是有序的，本质上是二叉树
 
 7 AVL 平衡二叉树
+struct node
+{
+    int v, height;
+    node* rchild,lchild;
+};
+node* newnode(int x)
+{
+    node* Node = new node;
+    Node->v = x;
+    Node->height = 1;
+    Node->lchild = Node->rchild = NULL;
+    return Node;
+}
+
+int getHeight(node* root)
+{
+    if(root==NULL)
+        return 0;
+    return root->height;
+}
+
+int getbalancedFactor(node* root)
+{
+    return getHeight(root->lchild) - getHeight(root->rchild);
+}
+
+void updateHeight(node* root)
+{
+    root->height = max(getHeight(root->rchild), getHeight(root->lchild));
+}
+查找 与二叉树类似
+
+插入 首先需要左旋以及右旋
+void L(node* &root)
+{
+    node *temp = root->rchild;
+    root->rchild = temp->lchild;
+    temp->lchild = root;
+    updateHeight(root);
+    updateHeight(temp);
+    root = temp;
+}
+
+void R(node* &root)
+{
+    node *temp = root->lchild;
+    root->lchild = temp->rchild;
+    temp->rchild = root;
+    updateHeight(root);
+    updateHeight(temp);
+    root = temp;
+}
+
+void insert(node* &root,int v)
+{
+    if(root==NULL)
+    {
+        root = newnode(v);
+        return;
+    }
+    if(v<root->v)
+    {
+        insert(root->lchild, v);
+        updateHeight(root);
+        if(getbalancedFactor(root)==2)
+        {
+            if(getbalancedFactor(root->lchild)==1)//LL
+            {
+                R(root);
+            }else if(getbalancedFactor(root->lchild)==-1)//LR
+            {
+                L(root->lchild);
+                R(root);
+            }
+        }
+    }
+    else
+    {
+        insert(root->rchild, v);
+        updateHeight(root);
+        if(getbalancedFactor(root)==-2)
+        {
+            if(getbalancedFactor(root->rchild)==-1)//RR
+            {
+                L(root);
+            }
+            else if(getbalancedFactor(root->rchild)==1)
+            {
+                R(root->rchild);
+                L(root);
+            }
+        }
+    }
+}
+AVL的建立
+    node* Create(int d[],int n)
+    {
+        node* root = NULL;
+        for (int i = 0; i < n;++i)
+        {
+            insert(root,data[i]);
+        }
+        return root;
+    }
 
 
 
-
-
-    
 
