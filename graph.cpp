a1f72d386f87c2455cd0aca70d3372a545952b7a
balance <32612349+justforbalance@users.noreply.github.com>
2019-02-24 21:49:11 +0800

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -124,3 +124,51 @@
         }
     }
     
+    增加pre数组的dijkstra算法
+    struct Node
+    {
+        int v, dis;
+    };
+    vector<Node> Adj[N];
+    int n;
+    int d[maxv];
+    int pre[maxv];
+    bool vis[maxv] = {false};
+
+    void Dijkstra(int s)
+    {
+        fill(d, d + maxv, INF);
+        for (int i = 0; i < n;++i)
+        {
+            pre[i] = i;
+        }
+        d[s] = 0;
+        for (int i = 0; i < n;++i)
+        {
+            int u = -1, Min = INF;
+            fof(int j = 0; j < n;++j)
+            {
+                if(vis[j]=false&&d[j]<Min)
+                {
+                    u = j;
+                    Min = dis[j];
+                }
+            }
+            if(u==-1)
+            {
+                return;
+            }
+            vis[u] = true;
+            for (int j = 0; j < Adj[u].size();++j)
+            {
+                int v = Adj[u][j].v;
+                if(vis[v]==false&&d[u]+Adj[u][j].dis<d[v])
+                {
+                    d[v] = d[u] + Adj[u][j].dis;
+                    pre[v] = u;
+                }
+            }
+        }
+    }
+
+    
