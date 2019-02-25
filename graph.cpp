cde9c265e35612f2efcb3f62b86ee7a34184092d
balance <32612349+justforbalance@users.noreply.github.com>
2019-02-25 20:40:48 +0800

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -123,7 +123,7 @@
             }
         }
     }
-    
+
     增加pre数组的dijkstra算法
     struct Node
     {
@@ -151,7 +151,7 @@
                 if(vis[j]=false&&d[j]<Min)
                 {
                     u = j;
-                    Min = dis[j];
+                    Min = d[j];
                 }
             }
             if(u==-1)
@@ -167,8 +167,55 @@
                     d[v] = d[u] + Adj[u][j].dis;
                     pre[v] = u;
                 }
+                如果上述条件相等，则判断第二尺度
             }
         }
     }
+
+    有时会出现除了最短路径之外的第二尺度，此时需要另外增加数组进行优化判断;
+    此外还有一种方法是首先使用dijkstra计算所有最优路径，在遍历这些路径进行第二尺度比较
+        代码如下：
+            vector<int> pre[maxv];
+            void dijkstra(int s)
+            {
+                fill(d, d + maxv, Inf);
+                d[s] = 0;
+                for (int i = 0; i < n;++i)
+                {
+                    int u = -1;
+                    int Min = Inf;
+                    for (int j = 0; j < n;++j)
+                    {
+                        if(vis[j]==false&&d[j]<Min)
+                        {
+                            u = j;
+                            min = d[j];
+                        }
+                    }
+                    if(u==-1)
+                    {
+                        return;
+                    }
+                    vis[u] = true;
+                    for (int j = 0; j < Adj[u].size();++j)
+                    {
+                        int v = Adj[u][j].v;
+                        if (vis[v] == false && d[u] + Adj[u][j].dis < d[v])
+                        {
+                            d[v] = d[u] + Adj[u][j].dis;
+                            pre[v].clear();
+                            pre[v].push_back(u);
+                        }
+                        else if(d[u]+Adj[u][j].dis==d[v])
+                        {
+                            pre[v].push_back(u);
+                        }
+                    }
+                }
+            }//所求的pre即不考虑第二尺度的最优路径，随后对这些路径进行遍历即可；
+            //使用DFS或者BFS遍历，使用vector进行纪录路径
+
+5.最小生成树
+    
 
     
